af1f7dfa5ac37cf632218b40cfbf9bc5c22e4220
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2022-05-27 18:46:46 +0900

    update load

--- src/load.js
+++ src/load.js
@@ -1,27 +1,28 @@
 const { google } = require("googleapis");
-const open = require("open");
-const http = require("http");
+const sheets = google.sheets("v4");
 const fs = require("fs");
 require("dotenv").config();
-const querystring = require("node:querystring");
-const url = require("node:url");
-
 const OAuth2 = google.auth.OAuth2;
-const oauth2Client = new OAuth2(
-  process.env.CLIENT_ID,
-  process.env.CLIENT_SECRET,
-  process.env.REDIRECT_URL
-);
-const token = await fs.promises.readFile(tokenPath);
 
-const spreadsheetId = process.env.PUBLIC_SHEETS_ID;
-const range = "A1:E10";
+(async () => {
+  const oauth2Client = new OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URL
+  );
 
-const apiOptions = {
-  auth: oauth2Client,
-  spreadsheetId,
-  range,
-};
+  const token = await fs.promises.readFile(process.env.TOKEN_FILE);
+  oauth2Client.setCredentials(JSON.parse(token));
 
-const sheet = await sheets.spreadsheets.values.get(apiOptions);
-console.log(sheet.data.values);
+  const spreadsheetId = process.env.PUBLIC_SHEETS_ID;
+  const range = "A1:E10";
+
+  const apiOptions = {
+    auth: oauth2Client,
+    spreadsheetId,
+    range,
+  };
+
+  const sheet = await sheets.spreadsheets.values.get(apiOptions);
+  console.log(sheet.data.values);
+})();
